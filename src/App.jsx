75b61a168c71c366e2984b4c2e37b6a53f62b291
Riya Rani <riyarani.core33@gmail.com>
2025-10-26 02:16:01 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 import './index.css'
 
 const App = () => {
@@ -11,6 +14,9 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
